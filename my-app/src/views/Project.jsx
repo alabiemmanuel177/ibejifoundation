8abe6752e5eb617d510aeeadbfb99f082295ad9b
Emmanuel Alabi <alabiemmanuel177@gmail.com>
2022-06-06 09:39:38 +0100

    Fixes on project page

--- my-app/src/views/Project.jsx
+++ my-app/src/views/Project.jsx
@@ -1,15 +1,66 @@
-import React from 'react'
-import { Carousel } from '../components/carousel/Carousel'
+import React from "react";
+import { Carousel } from "../components/carousel/Carousel";
+import './project.css'
 
 export const Project = () => {
   return (
-    <div>
-        <Carousel/>
-        <Carousel/>
-        <Carousel/>
-        <Carousel/>
-        <Carousel/>
-        <Carousel/>
+    <div className="Project">
+      <div className="miniproject">
+        <Carousel />
+        <div className="miniproject-text">
+          <h1>
+          January to December 2012 PROJECTS     
+          </h1>
+          <h3>
+          Many jobs were carried out from January to Decemberr 2012. We have touched various aspect of <br></br>humanitarian work which is outline and mention in details below
+          </h3>
+          <h3 className="bold">
+          Ajumoni central mosque Oworoshoki
+          </h3>
+        </div>        
+      </div>
+      <div className="miniproject">
+        <Carousel />
+        <div className="miniproject-text">
+          <h1>
+          January to December 2012 PROJECTS     
+          </h1>
+          <h3>
+          Many jobs were carried out from January to Decemberr 2012. We have touched various aspect of <br></br>humanitarian work which is outline and mention in details below
+          </h3>
+          <h3 className="bold">
+          Ajumoni central mosque Oworoshoki
+          </h3>
+        </div>        
+      </div>
+      <div className="miniproject">
+        <Carousel />
+        <div className="miniproject-text">
+          <h1>
+          January to December 2012 PROJECTS     
+          </h1>
+          <h3>
+          Many jobs were carried out from January to Decemberr 2012. We have touched various aspect of <br></br>humanitarian work which is outline and mention in details below
+          </h3>
+          <h3 className="bold">
+          Ajumoni central mosque Oworoshoki
+          </h3>
+        </div>        
+      </div>
+      <div className="miniproject">
+        <Carousel />
+        <div className="miniproject-text">
+          <h1>
+          January to December 2012 PROJECTS     
+          </h1>
+          <h3>
+          Many jobs were carried out from January to Decemberr 2012. We have touched various aspect of <br></br>humanitarian work which is outline and mention in details below
+          </h3>
+          <h3 className="bold">
+          Ajumoni central mosque Oworoshoki
+          </h3>
+        </div>        
+      </div>
     </div>
-  )
-}
+  );
+};
