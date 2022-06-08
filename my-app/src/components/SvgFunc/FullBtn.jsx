c2d01324a672ea10090c4c3b32d236096bf85756
Nasiru Iyidemilade <ladenas202@gmail.com>
2022-06-08 18:30:08 +0100

    post configuration

---
+++ my-app/src/components/SvgFunc/FullBtn.jsx
@@ -0,0 +1,56 @@
+import Svg1 from "./Svg1"
+import Svg2 from "./Svg2"
+import "./fullbtn.css"
+import Popup1 from "../Popup/Popup1";
+import Popup2 from "../Popup/Popup2";
+import {useState} from 'react';
+import React from "react";
+
+function FullBtn (){
+    const [buttonPopup, setButtonPopup] = useState(false);
+    const [buttonPopup1, sBtn] = useState(false);
+    return(
+        <div>
+            <div className="fullbtn">
+                  <button onClick={() => setButtonPopup(true)} className="spc"><Svg1></Svg1></button>
+                  <Popup1 trigger={buttonPopup} setTrigger={setButtonPopup}>
+                  <form>
+            <h2>Edit Employee Info</h2>
+        <div class="form-row">
+         <div class="form-group form-edit">
+              <label for="post-header">Post Header</label>
+              <input type="text" class="form-control input-edit" id="inputAddress" placeholder="Post Header"></input>
+          </div>
+          <div class="form-group">
+            <label for="inputPassword4">Post Summary</label>
+            {/* <input type="text" class="form-control" id="inputPassword4" placeholder="Last Name"></input> */}
+            <textarea className="txt-area " id="exampleFormControlTextarea1"></textarea>
+          </div>
+      </div>
+    <div class="form-group">
+            <label for="inputAddress2">Beneficiary</label>
+            <input type="text" class="form-control" id="inputAddress2"></input>
+        </div>
+        <div class="form-row">
+           <div class="input-group mb-3 mt-2">
+          <div class="custom-file">
+            <label>Post Image</label><br></br>
+            <input type="file" class="custom-file-input" id="inputGroupFile01"></input>
+          </div>
+        </div>
+        </div>
+        <button type="submit" class="btn btn-primary save-btn">Save</button>
+      </form>
+                  </Popup1>
+                  <button onClick={() => sBtn(true)} className="spc2"><Svg2></Svg2></button>
+                  <Popup2 trigger={buttonPopup1} setTrigger={sBtn}>
+                    <h3>Are you sure you want to delete this Post</h3>
+                    <button className="btn del-btn">Delete</button>
+                  </Popup2>
+            </div>
+        </div>
+    )
+
+}
+
+export default FullBtn;
--- my-app/src/views/Admin.jsx
+++ my-app/src/views/Admin.jsx
@@ -1,7 +1,106 @@
 import React from 'react'
+import "./admin.css"
+import {MdArrowDropDown} from 'react-icons/md'
+import FullBtn from '../components/SvgFunc/FullBtn'
+import {BsPeople} from 'react-icons/bs'
+import {AiOutlineTable} from 'react-icons/ai'
+
+
+
 
 export const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className='admin-holder'>
+              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+      <div className='sidebar1'>
+        <div className='user1'>
+        <h4>Ibeji Foundation</h4>
+        <img src='images/icon.png' className='user-icon' alt='icon'/>
+        <div class="dropdown">
+        <div className='logout-dropdown'><p>Mike Myers <MdArrowDropDown className="aIcon-pos"/></p></div>
+        <div class="dropdown-content">
+          <a>Logout</a>
+          <a>Change Password</a>
+        </div>
+        </div>
+       </div>
+       <hr></hr>
+       <div className='options'>
+          
+          <p className='option'><BsPeople className="o-icon"/><a>Applicants</a></p>
+          <p className='option'><AiOutlineTable className="o-icon"/><a>Posts</a></p>
+          <p className='option'><BsPeople className="o-icon"/><a>Applicants</a></p>
+          <p className='option'><AiOutlineTable className="o-icon"/><a>Posts</a></p>
+       </div>
+     
+      </div>
+      <div className='posts'>
+        <h1 className='page-head'>Post Configuration</h1>
+        <div className='table-holder2'>
+        <table class="table table-bordered table-width">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Post Header</th>
+      <th scope="col">Post Summary</th>
+      <th scope="col">Post Image </th>
+      <th scope="col">Beneficiary</th>
+      <th scope="col">Controls</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr className='wrap'>
+      <th scope="row">1</th>
+      <td>January to December 2012 PROJECTS</td>
+      <td className='hold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorum autem quaerat,
+         repellendus perspiciatis accusantium lo</td>
+      <td>Farm.jpg</td>
+      <td>Ibeji Foundation</td>
+      <td><FullBtn></FullBtn></td>
+      
+    </tr>
+    <tr>
+    <th scope="row">2</th>
+      <td>January to december stuff </td>
+      <td>Senior</td>
+      <td>Remote</td>
+      <td>2019</td>
+      <td><FullBtn></FullBtn></td>
+    
+    </tr>
+    <tr>
+    <th scope="row">3</th>
+      <td>January to december stuff</td>
+      <td>Senior</td>
+      <td>Remote</td>           
+      <td>2019</td>
+      <td><FullBtn></FullBtn></td>
+      
+    </tr>
+    <tr>
+    <th scope="row">4</th>
+      <td>Ugo Justice</td>
+      <td>Senior</td>
+      <td>Remote</td>
+      <td>2019</td>
+      <td><FullBtn></FullBtn></td>
+     
+    </tr>
+    <tr>
+    <th scope="row">5</th>
+      <td>Ugo Justice</td>
+      <td>Senior</td>
+      <td>Remote</td>
+      <td>2019</td>
+      <td><FullBtn></FullBtn></td>
+      
+    </tr>
+  </tbody>
+</table>
+</div>
+      </div>
+    </div>
   )
 }
